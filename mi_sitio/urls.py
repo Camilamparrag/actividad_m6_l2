0f646f98b1eec4519cac37381e3bee17b696d586
Camilamparrag <camilaparragon@gmail.com>
2026-05-13 01:59:27 +0000

    create app

--- mi_sitio/urls.py
+++ mi_sitio/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("principal/", include("principal.urls"))
-]
+    path("principal/", include("principal.urls")),
+    path("app/", include("app.urls")),
+    ]
+
